2bf1c6864e5a5c9f1b37bf41b1737d84757b01fd
markeckles <2435965705@qq.com>
2025-01-20 23:26:59 +0800

    提交

--- kv.go
+++ kv.go
@@ -10,18 +10,7 @@ import (
 )
 
 func Set[T any](key string, value T) (err error) {
-	bytes, err := serialize[T](value)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if err = db.Update(func(txn *badger.Txn) (err error) {
-		if err = txn.Set([]byte(key), bytes); err != nil {
-			log.Println(err)
-			return
-		}
-		return
-	}); err != nil {
+	if err = SetTtl[T](key, value, 0); err != nil {
 		log.Println(err)
 		return
 	}
@@ -35,7 +24,10 @@ func SetTtl[T any](key string, value T, second int) (err error) {
 		return
 	}
 	if err = db.Update(func(txn *badger.Txn) (err error) {
-		entry := badger.NewEntry([]byte(key), bytes).WithTTL(time.Duration(second) * time.Second)
+		entry := badger.NewEntry([]byte(key), bytes)
+		if second > 0 {
+			entry.WithTTL(time.Duration(second) * time.Second)
+		}
 		if err = txn.SetEntry(entry); err != nil {
 			log.Println(err)
 			return
@@ -48,22 +40,81 @@ func SetTtl[T any](key string, value T, second int) (err error) {
 	return
 }
 
-func Get[T any](key string) (value T, exists bool, err error) {
-	if err = db.View(func(txn *badger.Txn) (err error) {
-		var bytes []byte
-		if bytes, exists, err = get([]byte(key), txn); err != nil {
-			log.Println(err)
-			return
+func SetKey(key string) (err error) {
+	if err = SetKeyTtl(key, 0); err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func SetKeyTtl(key string, second int) (err error) {
+	bytes := make([]byte, 0)
+	if err = db.Update(func(txn *badger.Txn) (err error) {
+		entry := badger.NewEntry([]byte(key), bytes)
+		if second > 0 {
+			entry.WithTTL(time.Duration(second) * time.Second)
 		}
-		if !exists {
-			return
-		}
-		if value, err = deserialize[T](bytes); err != nil {
+		if err = txn.SetEntry(entry); err != nil {
 			log.Println(err)
 			return
 		}
 		return
 	}); err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func Exists(key string) (exists bool, err error) {
+	if err = db.View(func(txn *badger.Txn) (err error) {
+		_, err = txn.Get([]byte(key))
+		if errors.Is(err, badger.ErrKeyNotFound) {
+			err = nil
+			return
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		exists = true
+		return
+	}); err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func ExistsTtl(key string, second int) (exists bool, err error) {
+	if err = db.Update(func(txn *badger.Txn) (err error) {
+		k := []byte(key)
+		if _, err = txn.Get(k); errors.Is(err, badger.ErrKeyNotFound) {
+			err = nil
+			return
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		bytes := make([]byte, 0)
+		entry := badger.NewEntry(k, bytes).WithTTL(time.Duration(second) * time.Second)
+		if err = txn.SetEntry(entry); err != nil {
+			log.Println(err)
+			return
+		}
+		exists = true
+		return
+	}); err != nil {
+		log.Println(err)
+		return
+	}
+	return
+}
+
+func Get[T any](key string) (value T, exists bool, err error) {
+	if value, exists, err = GetTtl[T](key, 0); err != nil {
 		log.Println(err)
 		return
 	}
@@ -87,10 +138,12 @@ func GetTtl[T any](key string, second int) (value T, exists bool, err error) {
 			return
 		}
 
-		entry := badger.NewEntry(k, bytes).WithTTL(time.Duration(second) * time.Second)
-		if err = txn.SetEntry(entry); err != nil {
-			log.Println(err)
-			return
+		if second > 0 {
+			entry := badger.NewEntry(k, bytes).WithTTL(time.Duration(second) * time.Second)
+			if err = txn.SetEntry(entry); err != nil {
+				log.Println(err)
+				return
+			}
 		}
 
 		return
@@ -148,50 +201,18 @@ func Drop() (err error) {
 }
 
 func Storage[T any](key string, fn func() (value T, err error)) (value T, err error) {
-	if err = db.Update(func(txn *badger.Txn) (err error) {
-		var exists bool
-		var bytes []byte
-		k := Key(key)
-		if bytes, exists, err = get(k, txn); err != nil {
-			log.Println(err)
-			return
-		}
-
-		if exists {
-			if value, err = deserialize[T](bytes); err != nil {
-				log.Println(err)
-				return
-			}
-			return
-		}
-
-		if value, err = fn(); err != nil {
-			log.Println(err)
-			return
-		}
-
-		if bytes, err = serialize[T](value); err != nil {
-			log.Println(err)
-			return
-		}
-
-		if err = txn.Set(k, bytes); err != nil {
-			log.Println(err)
-			return
-		}
-		return
-	}); err != nil {
+	if value, err = StorageTtl[T](key, fn, 0); err != nil {
 		log.Println(err)
 		return
 	}
 	return
 }
 
-func StorageTtl[T any](key string, second int, fn func() (value T, err error)) (value T, err error) {
+func StorageTtl[T any](key string, fn func() (value T, err error), second int) (value T, err error) {
 	if err = db.Update(func(txn *badger.Txn) (err error) {
 		var exists bool
 		var bytes []byte
-		k := Key(key)
+		k := HashKey(key)
 		if bytes, exists, err = get(k, txn); err != nil {
 			log.Println(err)
 			return
@@ -214,10 +235,12 @@ func StorageTtl[T any](key string, second int, fn func() (value T, err error)) (
 			}
 		}
 
-		entry := badger.NewEntry(k, bytes).WithTTL(time.Duration(second) * time.Second)
-		if err = txn.SetEntry(entry); err != nil {
-			log.Println(err)
-			return
+		if second > 0 {
+			entry := badger.NewEntry(k, bytes).WithTTL(time.Duration(second) * time.Second)
+			if err = txn.SetEntry(entry); err != nil {
+				log.Println(err)
+				return
+			}
 		}
 		return
 	}); err != nil {
@@ -227,11 +250,11 @@ func StorageTtl[T any](key string, second int, fn func() (value T, err error)) (
 	return
 }
 
-func StorageTtlDiscord[T any](key string, second int, fn func() (value T, err error)) (value T, err error) {
+func StorageTtlDiscord[T any](key string, fn func() (value T, err error), second int) (value T, err error) {
 	if err = db.Update(func(txn *badger.Txn) (err error) {
 		var exists bool
 		var bytes []byte
-		k := Key(key)
+		k := HashKey(key)
 		if bytes, exists, err = get(k, txn); err != nil {
 			log.Println(err)
 			return
@@ -268,7 +291,7 @@ func StorageTtlDiscord[T any](key string, second int, fn func() (value T, err er
 	return
 }
 
-func Key(text string) (buf []byte) {
+func HashKey(text string) (buf []byte) {
 	n := xxhash.Sum64String(text)
 	buf = make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, n)
--- kv_test.go
+++ kv_test.go
@@ -81,7 +81,7 @@ func TestGet(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotValue, err := Get(tt.args.key)
+			gotValue, err := Exists(tt.args.key)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
 				return
