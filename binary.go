a48d4a8d499887e5cd535e506328c5bede703a0e
huazhimeng <2902151499@qq.com>
2025-01-22 21:05:35 +0800

    提交

--- binary.go
+++ binary.go
@@ -7,6 +7,11 @@ import (
 )
 
 func serialize[T any](data T) (b []byte, err error) {
+	b, ok := any(data).([]byte)
+	if ok {
+		return
+	}
+
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	if err = enc.Encode(data); err != nil {
@@ -18,6 +23,10 @@ func serialize[T any](data T) (b []byte, err error) {
 }
 
 func deserialize[T any](data []byte) (result T, err error) {
+	if _, ok := any(result).([]byte); ok {
+		return any(data).(T), nil
+	}
+
 	buf := bytes.NewBuffer(data)
 	dec := gob.NewDecoder(buf)
 	if err = dec.Decode(&result); err != nil {
